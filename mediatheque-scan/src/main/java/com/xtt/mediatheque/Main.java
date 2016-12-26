700c5c7852165335dbccdf7957c39327455a1746
Eric <Eric@192.168.1.41>
2016-12-26 14:39:47 +0100

    Add black words

--- mediatheque-scan/src/main/java/com/xtt/mediatheque/Main.java
+++ mediatheque-scan/src/main/java/com/xtt/mediatheque/Main.java
@@ -1,8 +1,5 @@
 package com.xtt.mediatheque;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,27 +16,7 @@ public class Main {
 		ApplicationContext context = new AnnotationConfigApplicationContext(
 				AppConfiguration.class);
 		MoviesScan moviesScan = context.getBean(MoviesScan.class);
-
-		System.out.println(System.getProperty("user.name"));
-		try {
-			System.out.println(InetAddress.getLocalHost().getHostName());
-		} catch (UnknownHostException e) {
-			System.err.println(e.getMessage());
-		}
-
-		// String directory = StringUtils.EMPTY;
-
-		// try {
-		// BufferedReader bufferRead = new BufferedReader(
-		// new InputStreamReader(System.in));
-		// directory = bufferRead.readLine();
-		//
-		// System.out.println(directory);
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-
-		String directory = "Y:\\Déja Vu";
+		String directory = "Y:\\Films";
 
 		if (!StringUtils.EMPTY.equalsIgnoreCase(directory)) {
 			moviesScan
