8a81d7835811e4d9f952ff1986a0a705a99d794c
Eric <Eric@Eric-PC>
2018-08-22 21:28:30 +0200

    Convert mediatheque utils from spring 4 to spring boot

--- mediatheque-utils/src/main/java/com/xtt/mediatheque/messages/MessageUtils.java
+++ mediatheque-utils/src/main/java/com/xtt/mediatheque/messages/MessageUtils.java
@@ -1,27 +1,20 @@
 package com.xtt.mediatheque.messages;
 
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.NoSuchMessageException;
-import org.springframework.stereotype.Component;
-
 import com.xtt.mediatheque.exceptions.MessageException;
 
-@Component
+//@Component
 public class MessageUtils {
 
-	@Autowired
-	private MessageSource messageSource;
+//	@Autowired
+//	private MessageSource messageSource;
 
 	public String getMessageWithParameters(String key, String[] args) throws MessageException {
 		String message = "";
-		try {
-			message = messageSource.getMessage(key, args, Locale.FRENCH);
-		} catch (NoSuchMessageException e) {
-			throw new MessageException(e.getMessage());
-		}
+//		try {
+//			message = messageSource.getMessage(key, args, Locale.FRENCH);
+//		} catch (NoSuchMessageException e) {
+//			throw new MessageException(e.getMessage());
+//		}
 		return message;
 	}
 
