ca03e649f13adb79ebde26b2b34a4d925c112da6
Eric <Eric@Eric-PC>
2018-10-07 19:29:25 +0200

    Mise en place de Lombok

--- mediatheque-dto/src/main/java/com/xtt/mediatheque/dto/CatalogItemDTO.java
+++ mediatheque-dto/src/main/java/com/xtt/mediatheque/dto/CatalogItemDTO.java
@@ -2,66 +2,21 @@ package com.xtt.mediatheque.dto;
 
 import java.util.Date;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CatalogItemDTO {
 
 	private String urlCover;
 
 	private String title;
 
-	private String releaseYear;
+	private long releaseYear;
 
 	private Date addingDate;
 
-	private long idAllocine;
-
-	private String userName;
-
-	public String getUrlCover() {
-		return urlCover;
-	}
-
-	public void setUrlCover(String urlCover) {
-		this.urlCover = urlCover;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getReleaseYear() {
-		return releaseYear;
-	}
-
-	public void setReleaseYear(String releaseYear) {
-		this.releaseYear = releaseYear;
-	}
-
-	public Date getAddingDate() {
-		return addingDate;
-	}
-
-	public void setAddingDate(Date addingDate) {
-		this.addingDate = addingDate;
-	}
-
-	public long getIdAllocine() {
-		return idAllocine;
-	}
-
-	public void setIdAllocine(long idAllocine) {
-		this.idAllocine = idAllocine;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	private long id;
 
 }
