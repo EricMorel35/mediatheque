b184dea35a1b68406d7d5e9ed51fbe35b67cc168
Eric <Eric@Eric-PC>
2018-10-07 19:37:42 +0200

    Ajout du fichier data.sql

---
+++ mediatheque-webapp/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+create table Movies_User (
+   id integer not null,
+   movie_name varchar(255) not null,
+   original_name varchar(255) not null,
+   movie integer,
+   primary key(id)
+);
+
+create table Movies (
+   movie_id integer not null,
+   movie_title varchar(255) not null,
+   synopsis varchar(255) not null,
+   release_year integer not null,
+   url_cover varchar(255) not null,
+   url_youtube varchar(255) not null,
+   movie_user_id integer,
+   primary key(movie_id)
+);
+
+INSERT INTO Movies_User VALUES (1,'Iron Man', 'Iron Man',0);
