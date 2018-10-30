89b49ff3debacbcb320806be807461acb56ec74f
Eric Morel <emorel@sii.fr>
2018-10-30 17:52:57 +0100

    Ajout de la table kind

--- mediatheque-webapp/src/main/resources/data.sql
+++ mediatheque-webapp/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-create table if not exists Movies_User (
+create table if not exists Movie_User (
    id integer not null,
    movie_name varchar(255) not null,
    original_name varchar(255) not null,
@@ -6,7 +6,7 @@ create table if not exists Movies_User (
    primary key(id)
 );
 
-create table if not exists Movies (
+create table if not exists Movie (
    movie_id integer not null,
    movie_title varchar(255) not null,
    synopsis clob not null,
@@ -17,5 +17,9 @@ create table if not exists Movies (
    primary key(movie_id)
 );
 
-INSERT INTO Movies_User (id,movie_name,original_name,movie) VALUES (1,'Iron Man','Iron Man',0);
-INSERT INTO Movies_User (id,movie_name,original_name,movie) VALUES (2,'Taxi','Taxi',0);
+create table if not exists Movie_Kind (
+   kind varchar(50) not null,
+);
+
+INSERT INTO Movie_User (id,movie_name,original_name,movie) VALUES (1,'Iron Man','Iron Man',0);
+INSERT INTO Movie_User (id,movie_name,original_name,movie) VALUES (2,'Taxi','Taxi',0);
